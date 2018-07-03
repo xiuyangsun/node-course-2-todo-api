538ac319c77e100975bf2f0b251fe16180ad933a
Bobby Sun <xsun785@gmail.com>
2018-07-03 19:15:26 -0400

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //When using heroku 'mongodb://bobbysun8910:bobbysunabc123@ds125851.mlab.com:25851/todoapp' ||
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
-module.exports = {mongoose};
+module.exports.mongoose = mongoose;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 
-var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {mongoose} = require('./db/mongoose');
 var app = express();
-var port = process.env.PORT ||3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
